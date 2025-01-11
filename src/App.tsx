2bdcbd8334813faf420e75016cf8ead98b90780f
zhenhao23 <weezhenhao@gmail.com>
2025-01-11 11:52:27 +0800

    vercel test

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import SolarPage from "./SolarPage/SolarPage.tsx";
 import ProfilePage from "./ProfilePage/ProfilePage.tsx";
 // import WelcomePage from "./LoginRegisterPage/WelcomePage.tsx";
 // import WelcomeBackground from "./LoginRegisterPage/WelcomeBackground.tsx";
+// hello
 
 function App() {
   return (
